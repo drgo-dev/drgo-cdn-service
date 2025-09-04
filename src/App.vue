5c068388c4adfdc91974e1cfb7ffcb3dcdaa8707
drgo-dev <devcon@drgo.pro>
2025-09-04 10:29:25 +0900

    variable fix

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
-<template>
-  <LoginView v-if="!isLoggedIn" />
-  <SignatureView v-else />
-</template>
-
 <script setup>
 import { computed } from 'vue'
 import { useUserStore } from '@/stores/user'
@@ -10,4 +5,13 @@ import LoginView from '@/views/LoginView.vue'
 import SignatureView from '@/views/Signature.vue'
 const isLoggedIn = computed(() => useUserStore().isLoggedIn)
 </script>
+
+
+
+<template>
+  <div style="padding:40px">프론트 렌더 OK</div>
+  <LoginView v-if="!isLoggedIn" />
+  <SignatureView v-else />
+</template>
+
 
